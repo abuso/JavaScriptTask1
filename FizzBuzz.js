239d0faaf66bd41715dcf312f2f038e6e13fc0c1
DESKTOP-6LCURLL\Yanagita <yosiphi@hotmail.com>
2021-12-04 22:13:52 +0900

    2021/12/4修正

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,6 +1,5 @@
 function FizzBuzz() {
     for(i=1; i<=100; i++) {
- //       const ouput = document.getElementById("#output");
         if(i%3 === 0 && i%5 === 0) {
             console.log(`FizzBuzz ${i}`);
         }else if(i%3 === 0) {
